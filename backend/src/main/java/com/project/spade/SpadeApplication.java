bda875349e4538bc6b4e28ef3786252efedfc3b6
Dinko Gekov <dinko.gekov@gmail.com>
2025-01-28 23:12:04 +0200

    initial commit

---
+++ backend/src/main/java/com/project/spade/SpadeApplication.java
@@ -0,0 +1,13 @@
+package com.project.spade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpadeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/spade/SpadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.spade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
