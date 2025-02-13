ee7bfac1e296287d87ba78dec2dee31e0c9b2460
Dinko Gekov <dinko.gekov@gmail.com>
2025-02-14 01:22:01 +0200

    impl crud operations for user

--- backend/src/main/java/com/project/spade/controllers/UserController.java
+++ backend/src/main/java/com/project/spade/controllers/UserController.java
@@ -1,7 +1,71 @@
 package com.project.spade.controllers;
 
+import com.project.spade.dtos.UserDTO;
+import com.project.spade.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody UserDTO userDTO){
+
+        UserDTO createdUser = this.userService.createUser(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully.");
+    }
+
+    @GetMapping
+    public List<UserDTO> getAllUsers(){
+
+//        List<UserDTO> result = this.userService.getAllUsers()
+
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getSingleUser(@PathVariable int id){
+        Optional<UserDTO> responseUser = this.userService.getSingleUser(id);
+
+        if(responseUser == null){
+            return (ResponseEntity<?>) ResponseEntity.status(HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable int id, @RequestBody UserDTO userDTO){
+        boolean isUpdateSuccessful = this.userService.updateUser(id, userDTO);
+
+        if(isUpdateSuccessful){
+            return ResponseEntity.status(HttpStatus.OK).body("Username updated successfully");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable int id){
+        boolean isUserDeleted = this.userService.deleteUser(id);
+
+        if(!isUserDeleted){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
+    }
 }
--- backend/src/main/java/com/project/spade/services/UserService.java
+++ backend/src/main/java/com/project/spade/services/UserService.java
@@ -1,7 +1,72 @@
 package com.project.spade.services;
 
+import com.project.spade.dtos.UserDTO;
+import com.project.spade.entities.User;
+import com.project.spade.mappers.UserMapper;
+import com.project.spade.repositories.UserRepo;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
+    private final UserRepo userRepo;
+    private final UserMapper userMapper;
+
+    public UserService(UserRepo userRepo, UserMapper userMapper) {
+        this.userRepo = userRepo;
+        this.userMapper = userMapper;
+    }
+
+    public UserDTO createUser(UserDTO userDTO){
+        if(userRepo.existsByUsername(userDTO.getUsername())){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Username already taken");
+        }
+
+        User user = userMapper.toEntity(userDTO);
+        User savedUser = userRepo.save(user);
+        return userMapper.toDto(savedUser);
+    }
+
+    public List<UserDTO> getAllUsers(){
+        return userRepo.findByIsActive(1).stream().map(userMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<UserDTO> getSingleUser(int id){
+        Optional<User> wrappedUser = Optional.ofNullable(userRepo.findByIdAndIsActive(id, 1));
+
+        return wrappedUser.map(userMapper::toDto);
+    }
+
+    public boolean updateUser(int id, UserDTO userDTO){
+        User user = userRepo.findByIdAndIsActive(id, 1);
+        if(user != null){
+            user.setUsername(userDTO.getUsername());
+            User updatedUser = userRepo.save(user);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteUser(int id){
+        User user = userRepo.findByIdAndIsActive(id, 1);
+        if(user != null){
+            user.setIsActive(0);
+            user.setUsername(user.getUsername() + "_deleted" + user.getId());
+            this.userRepo.save(user);
+            return true;
+        }
+
+        // added the _deleted{id} thing for the username in case
+        // some other user wants to use the username as it would be kinda silly
+        // to give someone else the error username already taken while the user
+        // with that username being deleted
+
+        return false;
+    }
 }
